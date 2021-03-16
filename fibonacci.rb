424df524ced107917cc23a7449da28a02eecb9fa
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-03-17 00:42:47 +0900

    my_answer

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,14 @@
 class FibonacciSequence
   def self.fibonacci_item(n)
-    0
+    FibonacciSequence.fib(n, {0 => 0, 1 => 1})
+  end
+
+  def self.fib(n, hash)
+    if hash[n]
+      hash[n]
+    else
+      hash[n] = FibonacciSequence.fib(n - 1, hash) + FibonacciSequence.fib(n - 2, hash)
+    end
   end
 end
 
